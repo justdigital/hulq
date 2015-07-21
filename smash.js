5999afb221ef6e9f8d1613617f057cdc744a633d
lucas.chain@justdigital.com.br <lucas.chain@justdigital.com.br>
2015-07-21 14:12:29 -0300

    Some filenames changed for better understanding and added a error message for unavailable xslt on smash command

---
+++ smash.js
@@ -0,0 +1,28 @@
+var fs      = require("fs");
+var args = process.argv.slice(2);
+if (!args[0]){
+  console.log("Please input a filename");
+  return;
+}
+
+var createFile = function(name, content){
+  fs.writeFile("./generated/" + name + ".xslt", content, function(err) {
+    if (err) {console.log(err); return;}
+  });
+}
+
+var resourceFilePath = "./resources/" + args[0] + ".xslt";
+
+fs.readFile(resourceFilePath, {encoding: "utf8"}, function(err, templateContent){
+  if (err){
+    console.log("The XSLT file was not available: (" + resourceFilePath + ")");
+    return;
+  }
+  templateContent = templateContent.replace(/<xsl:template\s+name="(.*)">((?:[\s\S])*?)<\/xsl:template>/g, function(match, p1, p2, offset, string){
+    createFile("partials/" + p1.toLowerCase(), p2.trim());
+    return match.replace(p2, "{{" + p1 + "}}");
+  });
+
+  createFile("template", templateContent);
+  console.log("Done.");
+});

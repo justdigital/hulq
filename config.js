849518cc701ebb6c36a6d115f7d04e590a3fc171
lucas.chain@justdigital.com.br <lucas.chain@justdigital.com.br>
2015-08-07 20:08:11 -0300

    Config comments to provide a better understanding

--- config.js
+++ config.js
@@ -1,31 +1,72 @@
 module.exports = {
+  /*
+   * GSA Panel protocol: http or https
+   * Only needed for the deploy function
+   */
+  gsaProtocol: 'https',
+  
+  /*
+   * GSA Panel host
+   * Only needed for the deploy function
+   */
+  gsaHost: '10.0.16.65',
 
-  gsaProtocol:      'https',
+  /*
+   * GSA Panel port
+   * Only needed for the deploy function
+   */
+  gsaPanelPort: '8443',
 
-  gsaHost:          '10.0.16.65',
+  /*
+   * GSA Panel login (username)
+   * Only needed for the deploy function
+   */
+  gsaLogin: 'admin',
 
-  gsaPanelPort:     '8443',
+  /*
+   * GSA Panel password
+   * Only needed for the deploy function
+   */
+  gsaPass: 'gsa123',
 
-  gsaLogin:         'admin',
+  /*
+   * JS files path
+   */
+  jsPath: './assets/js',
+ 
+  /*
+   * JS files output path (where to go after gulp's js task)
+   */
+  jsBuildPath: './assets/build/js',
 
-  gsaPass:          'gsa123',
+  /*
+   * CSS files path
+   */
+  cssPath: './assets/css',
 
-  jsPath:           './assets/js',
-
-  jsBuildPath:      './assets/build/js',
-
-  cssPath:          './assets/css',
-
+  /*
+   * CSS files output path
+   */
   cssBuildPath:     './assets/build/css',
 
-  generatedPath:    './generated',
+  /*
+   * Generated files path (template and partials)
+   * This path is used when you do a hulq smash. Generated files go to this path
+   */
+  generatedPath:          './generated',
+  generatedPartialsPath:  './generated/partials',
 
-  generatedPartialsPath:     './generated/partials',
+  /*
+   * Workspace path (template and partials)
+   * This is where you are going to work
+   */
+  xsltPath:         './workspace',
+  xsltPartialsPath: './workspace/partials',
 
-  xsltPath:         './xslt',
-
-  xsltPartialsPath: './xslt/partials',
-
-  xsltBuildPath:    './xslt/build'
+  /*
+   * Output path
+   * Where the final generated xslt will go after ./hulq recover
+   */
+  xsltBuildPath:    './build'
 
 };
